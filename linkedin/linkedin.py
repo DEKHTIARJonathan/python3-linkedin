68e99db624aa66933f4267de3d09cb8e6f4bf6ab
Emlyn Clay <eclay101@gmail.com>
2016-04-25 15:06:01 +0100

    Remove linkedin permissions that are no longer available

--- linkedin/linkedin.py
+++ linkedin/linkedin.py
@@ -27,13 +27,8 @@ allowed.  I will put the ones here in the comments that I have removed:
 PERMISSIONS = enum('Permission',
                    COMPANY_ADMIN='rw_company_admin',
                    BASIC_PROFILE='r_basicprofile',
-                   FULL_PROFILE='r_fullprofile',
                    EMAIL_ADDRESS='r_emailaddress',
-                   NETWORK='r_network',
-                   CONTACT_INFO='r_contactinfo',
-                   NETWORK_UPDATES='rw_nus',
-                   GROUPS='rw_groups',
-                   MESSAGES='w_messages')
+                   SHARE='w_share')
 
 ENDPOINTS = enum('LinkedInURL',
                  PEOPLE='https://api.linkedin.com/v1/people',

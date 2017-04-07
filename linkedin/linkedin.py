5dfa3627c131bc6bb199b32f259de5170b656793
Jonathan DEKHTIAR <DEKHTIARJonathan@users.noreply.github.com>
2017-04-07 20:18:18 +0200

    get_post_likes added

--- linkedin/linkedin.py
+++ linkedin/linkedin.py
@@ -566,6 +566,15 @@ class LinkedInApplication(object):
         raise_for_error(response)
         return True
 
+    def get_post_likes(self, post_id, selectors=None, params=None, headers=None):
+        url = '%s/%s/likes' % (ENDPOINTS.POSTS, post_id)
+        if selectors:
+            url = '%s:(%s)' % (url, LinkedInSelector.parse(selectors))
+
+        response = self.make_request('GET', url, params=params, headers=headers)
+        raise_for_error(response)
+        return response.json()
+
     def get_company_historical_status_update_statistics(self, company_id, params=None, headers=None):
         url = '%s/%s/historical-status-update-statistics' % (ENDPOINTS.COMPANIES, str(company_id))
         response = self.make_request('GET', url, params=params, headers=headers)

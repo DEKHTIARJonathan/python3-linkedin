885d2de6a99b227f6cf4bedc957fc9250c04b019
Jonathan DEKHTIAR <DEKHTIARJonathan@users.noreply.github.com>
2017-04-07 20:19:02 +0200

    get_share_comments added

--- linkedin/linkedin.py
+++ linkedin/linkedin.py
@@ -575,6 +575,17 @@ class LinkedInApplication(object):
         raise_for_error(response)
         return response.json()
 
+    def get_share_comments(self, post_id, selectors=None, params=None,
+                           headers=None):
+        url = '%s/~/network/updates/key=%s/update-comments' % (ENDPOINTS.PEOPLE,
+                                                               post_id)
+        if selectors:
+            url = '%s:(%s)' % (url, LinkedInSelector.parse(selectors))
+
+        response = self.make_request('GET', url, params=params, headers=headers)
+        raise_for_error(response)
+        return response.json()
+
     def get_company_historical_status_update_statistics(self, company_id, params=None, headers=None):
         url = '%s/%s/historical-status-update-statistics' % (ENDPOINTS.COMPANIES, str(company_id))
         response = self.make_request('GET', url, params=params, headers=headers)

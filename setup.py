978949c611f78ca00b4ddb1c17fadc167e38b8dd
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2017-04-08 17:37:39 +0200

    Travis Deploy Script Added Setup.py modified

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ except ImportError:
 
 from linkedin import __version__
 
-
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     long_description = readme.read()
 
@@ -22,17 +21,19 @@ setup(name='python3-linkedin',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
           'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Natural Language :: English',
       ],
-      keywords='linkedin python',
+      keywords='linkedin python python3',
       author='Ozgur Vatansever',
       author_email='ozgurvt@gmail.com',
-      maintainer='Marshall Humble',
-      maintainer_email='humblejm@gmail.com',
+      maintainer='Jonathan Dekhtiar',
+      maintainer_email='contact@jonathandekhtiar.eu',
       url='https://github.com/DEKHTIARJonathan/python3-linkedin',
       license='MIT',
       packages=['linkedin'],

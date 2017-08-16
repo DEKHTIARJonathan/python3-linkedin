4073d799af5b73acb061f6a4cb833d384637a835
Jonathan DEKHTIAR <DEKHTIARJonathan@users.noreply.github.com>
2017-08-16 21:24:27 +0200

    Update authentication.py

--- examples/authentication.py
+++ examples/authentication.py
@@ -1,12 +1,17 @@
-from linkedin.linkedin import (LinkedInAuthentication, LinkedInApplication,
-                               PERMISSIONS)
-
+from linkedin.linkedin import (LinkedInAuthentication, LinkedInApplication, PERMISSIONS)
 
 if __name__ == '__main__':
-    API_KEY = 'wFNJekVpDCJtRPFX812pQsJee-gt0zO4X5XmG6wcfSOSlLocxodAXNMbl0_hw3Vl'
-    API_SECRET = 'daJDa6_8UcnGMw1yuq9TjoO_PMKukXMo8vEMo7Qv5J-G3SPgrAV0FqFCd0TNjQyG'
-    RETURN_URL = 'http://localhost:8000'
-    authentication = LinkedInAuthentication(API_KEY, API_SECRET, RETURN_URL,
-                                            list(PERMISSIONS.enums.values()))
+  
+    CLIENT_ID     = '############'
+    CLIENT_SECRET = '#############'
+    RETURN_URL = 'http://localhost:8000/callback/'
+    
+    authentication = LinkedInAuthentication(
+                        CLIENT_ID,
+                        CLIENT_SECRET,
+                        RETURN_URL,
+                        PERMISSIONS.enums.values()
+                    )
+    
     print(authentication.authorization_url)
     application = LinkedInApplication(authentication)
